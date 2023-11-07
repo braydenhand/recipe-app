4c04d3d12d5d24bdd0e46278f7947bbebfe6c291
Joseph Ferry <jferry@berkeley.edu>
2023-11-07 21:23:04 +0100

    added relationships and indgredient form

--- model.py
+++ model.py
@@ -7,12 +7,6 @@ recipe_ingredients = db.Table('recipe_ingredients',
     db.Column('recipe_id', db.Integer, db.ForeignKey('recipe.id')),
     db.Column('ingredient_id', db.Integer, db.ForeignKey('qingredient.id'))
 )
-#I am a little confused on what exactly should occur between our tables. Can someone 
-#talk with him during lab about exactly what tables should backpopulate what and why? 
-# Also why do we need an ingredients and qingredients table? Can we not define the ingredient 
-# with a quantity each time? 
-
-
 
 class User(flask_login.UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -20,6 +14,10 @@ class User(flask_login.UserMixin, db.Model):
     name = db.Column(db.String(64), nullable=False)
     password = db.Column(db.String(100), nullable=False)
     recipes = db.relationship('Recipe', back_populates='user')
+    ratings = db.relationship('Rating', back_populates='user')
+    steps = db.relationship('Step', back_populates='user')
+    photos = db.relationship('Photo', back_populates='user')
+    bookmarks = db.relationship('Bookmark', back_populates='user')
 
 class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -29,38 +27,60 @@ class Recipe(db.Model):
     cooking_time = db.Column(db.Integer, nullable=False)
     number_people = db.Column(db.Integer, nullable=False)
     timestamp = db.Column(db.DateTime(), nullable=False)
- 
-   #Need to add relationship to QIngredients and Steps 
+    ingredients = db.relationship('Ingredient', back_populates='recipe')
+    ratings = db.relationship('Rating', back_populates='recipe')
+    steps = db.relationship('Step', back_populates='recipe')
+    photos = db.relationship('Photo', back_populates='recipe')
+    bookmarks = db.relationship('Bookmark', back_populates='recipe')
 
 class Ingredient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(512), nullable=False)
-
-#We either need to add a relationship where recipe_id backpopulates recipe with a column involving the quantity
-#or we use a nice query later on. Something like SELECT * FROM QIngredient WHERE recipe_id == Recipe.id (my preference) 
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
+    recipe = db.relationship('Recipe', back_populates='ingredients')
+    qingredients = db.relationship('QIngredient', back_populates='ingredient')
 
 
 class QIngredient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(512), nullable=False)
-    recipe_id = db.Column(db.Integer, nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
     unit = db.Column(db.String(512), nullable=False)
+    ingredient_id = db.Column(db.Integer, db.ForeignKey('ingredient.id'), nullable=False)
+    ingredient = db.relationship('Ingredient', back_populates='qingredients')
 
 class Step(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(512), nullable=False)
-    recipe_id = db.Column(db.Integer, nullable=False)
     position = db.Column(db.Integer, nullable=False)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
+    recipe = db.relationship('Recipe', back_populates='steps')
 
 #If we are supposed to see a User's ratings on their profile page, we should backpopulate 
 #the User with the rating ID.  
 
 class Rating(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    recipe_id = db.Column(db.Integer, nullable=False)
-    user_id = db.Column(db.Integer, nullable=False)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
+    recipe = db.relationship('Recipe', back_populates='ratings')
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', back_populates='ratings')
     value = db.Column(db.Integer, nullable=False)
-#Let's not worry about the photo table for now. 
 
-    
+class Photo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
+    recipe = db.relationship('Recipe', back_populates='photos')
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', back_populates='photos')
+    #need to determine how to store photo
+    value = db.Column(db.Integer, nullable=False)    
+
+
+#need to add bookmarked entities
+class Bookmark(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
+    recipe = db.relationship('Recipe', back_populates='bookmarks')
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', back_populates='bookmarks')
+    value = db.Column(db.Integer, nullable=False)
