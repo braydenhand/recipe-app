876d9a9ef53d445e7991ae474543a7c08590d9e1
Joseph Ferry <jferry@berkeley.edu>
2023-11-20 13:43:16 +0100

    remove follow feature used in twitter app

--- recipe/main.py
+++ recipe/main.py
@@ -11,18 +11,6 @@ bp = Blueprint("main", __name__)
 
 @bp.route("/")
 def index():
-    """
-    followers = db.aliased(model.User)
-    query = (
-    db.select(model.Message)
-    .join(model.User)
-    .join(followers, model.User.followers)
-    .where(followers.id == flask_login.current_user.id)
-    .where(model.Message.response_to == None)
-    .order_by(model.Message.timestamp.desc())
-    .limit(10)
-)
-"""
     query = (
         db.select(model.Recipe)
         .limit(10)#can adjust this later
@@ -49,16 +37,6 @@ def profile(user_id):
     ratings = db.session.execute(query).scalars().all()
     query = db.select(model.Bookmark).where(model.Bookmark.user_id == user_id)
     bookmarks = db.session.execute(query).scalars().all()
-    '''
-    if flask_login.current_user.id == user_id:
-        followValue=None
-    elif flask_login.current_user in user.followers:
-        followValue="unfollow"
-    else:
-        followValue="follow"
-    following = user.following
-    followers = user.followers
-    '''
     return render_template('main/profile.html',user=user, recipes=recipes, ratings=ratings, bookmarks=bookmarks)
 
 #we will need recipes, steps, photos, ingredients, and ratings
@@ -173,38 +151,3 @@ def upload_photo(recipe_id):
 
     abort(400, "No file uploaded")
 '''
-
-#no use case for this function yet, possibly can be used for staying up to date on following certain users
-@bp.route("/follow/<int:user_id>", methods=["POST"])
-@flask_login.login_required
-def follow(user_id):
-    user = db.session.get(model.User, user_id)
-    if not user:
-        abort(404, "User id {} doesn't exist.".format(user_id))
-    if flask_login.current_user in user.followers:
-        abort(403, "Already following user.")
-    user.followers.append(flask_login.current_user)
-    query = db.select(model.Message).where(model.Message.response_to == None).where(model.Message.user_id == user_id).order_by(model.Message.timestamp.desc()).limit(10)
-    posts = db.session.execute(query).scalars().all()
-    db.session.commit()
-    following = user.following
-    followers = user.followers
-    return render_template('main/profile.html',user=user, posts=posts,follow_button="unfollow",followers=followers, following=following)
-    
-#no use case yet    
-@bp.route("/unfollow/<int:user_id>", methods=["POST"])
-@flask_login.login_required
-def unfollow(user_id):
-    user = db.session.get(model.User, user_id)
-    if not user:
-        abort(404, "User id {} doesn't exist.".format(user_id))
-    if flask_login.current_user not in user.followers:
-        abort(403, "Not following user.")
-    user.followers.remove(flask_login.current_user)
-    query = db.select(model.Message).where(model.Message.response_to == None).order_by(model.Message.timestamp.desc()).limit(10)
-    posts = db.session.execute(query).scalars().all()
-    db.session.commit()
-    return render_template("main/index.html", posts=posts)
-
-
-
